6a5dfc42730ea82343fccf9f30e06a3ebfce3aed
Arnaud Garin <arnaud_lhc@hotmail.com>
2019-05-09 23:12:56 +0200

    COUNT aggregation implementation

--- src/main/scala/cubeoperator/CubeOperator.scala
+++ src/main/scala/cubeoperator/CubeOperator.scala
@@ -31,8 +31,22 @@ class CubeOperator(reducers: Int) {
     val single_lines = rdd.map(x => ((index.map(y => x.get(y))), x.get(indexAgg).asInstanceOf[Int])).map(x => (x._1.mkString(", ").replace("Some(", "").replace(")", ""), x._2))
 
     agg match {
-      case "COUNT" => // TODO implementation
-        return null;
+      case "COUNT" =>
+        val single_lines = rdd.map(x => ((index.map(y => x.get(y))), 1)).groupBy(_._1).mapValues(_.map(_._2).sum)
+
+        val single_stringed = single_lines.map(x => (x._1.mkString(", ").replace("Some(", "").replace(")", ""), x._2))
+
+        val partition_step_one = rdd_partition.mapPartitions( pa => pa.map(x => ((index.map(y => x.get(y))), 1))).groupBy(_._1).mapValues(_.map(_._2).sum)
+
+        val partition_partial_upper = partition_step_one.mapPartitions( part => part.map(x => perms.map(p => (x._1.zipWithIndex.map{case(e, i) => if(p contains i) Some(e) else None}, x._2))).flatMap(x => x))
+
+        val partial_cubic = partition_partial_upper.groupBy(_._1).mapValues(_.map(_._2).sum)
+
+        val cubic = partial_cubic.repartition(1).groupBy(_._1).mapValues(_.map(_._2).sum)
+
+        val ret = cubic.map(x => (x._1.mkString(", ").replace("Some(", "").replace(")", ""), x._2)).union(single_stringed).map(x => (x._1, x._2.toDouble))
+
+        return ret
 
       case "SUM" =>
         val single_lines = rdd.map(x => ((index.map(y => x.get(y))), x.get(indexAgg).asInstanceOf[Int])).groupBy(_._1).mapValues(_.map(_._2).sum)
@@ -84,7 +98,7 @@ class CubeOperator(reducers: Int) {
         val ret = cubic.map(x => (x._1.mkString(", ").replace("Some(", "").replace(")", ""), x._2)).union(single_stringed).map(x => (x._1, x._2.toDouble))
 
         return ret
-        
+
       case "AVG" => // TODO implementation
         return null;
       case _ => return null;
