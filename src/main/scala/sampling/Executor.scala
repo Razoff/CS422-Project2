15ddc2fe627fb10aa09b6d2e92a1bf91f0b48c0c
Arnaud Garin <arnaud_lhc@hotmail.com>
2019-05-18 02:09:52 +0200

    Query 9

--- src/main/scala/sampling/Executor.scala
+++ src/main/scala/sampling/Executor.scala
@@ -157,7 +157,29 @@ object Executor {
   }
 
   def execute_Q9(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 1)
+
+    val like : String = params(0).asInstanceOf[String]
+
+    get_lineitem_df(desc, session, List(1,2,3)).createOrReplaceTempView("lineitem")
+    desc.part.createOrReplaceTempView("part")
+    desc.supplier.createOrReplaceTempView("supplier")
+    desc.partsupp.createOrReplaceTempView("partsupp")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.nation.createOrReplaceTempView("nation")
+
+    session.sql(
+      "select n_name as nation, year(o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount " +
+        "from part, supplier, lineitem, partsupp, orders, nation " +
+        "where s_suppkey = l_suppkey and ps_suppkey = l_suppkey and ps_partkey = l_partkey and p_partkey = l_partkey and o_orderkey = l_orderkey and s_nationkey = n_nationkey and p_name like '%" + like + "%'"
+    ).createOrReplaceTempView("profit")
+
+    session.sql(
+      "select nation, o_year, sum(amount) as sum_profit " +
+        "from profit " +
+        "group by nation, o_year " +
+        "order by nation, o_year desc"
+    ).show()
   }
 
   def execute_Q10(desc: Description, session: SparkSession, params: List[Any]) = {
